0ca5d4394887a15f5e707441210b269fb07ba677
2sf <2sfkarim@gmail.com>
2021-10-19 18:57:34 +0600

    refactored homepage into another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:recipes/recipe.dart';
-import 'recipe_detail.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -21,72 +20,6 @@ class RecipeApp extends StatelessWidget {
         ),
       ),
       home: const MyHomePage(title: 'Recipe'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: Recipe.samples.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return RecipeDetail(recipe: Recipe.samples[index]);
-                    },
-                  ),
-                );
-              },
-              child: buildRecipeCard(Recipe.samples[index]),
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget buildRecipeCard(Recipe recipe) {
-    return Card(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Image(image: AssetImage(recipe.imageUrl)),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              recipe.label,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino',
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
